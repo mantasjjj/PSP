81cf19f4a22433060ac59ecea8c0a0d4f228e071
mantasjjj <mantas.jakaitis@mif.stud.vu.lt>
2021-09-13 21:14:55 +0300

    add: tests for Email, Password and PhoneNumber validators

---
+++ ValidationModule/test/EmailValidatorTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EmailValidatorTest {
+
+    EmailValidator emailValidator;
+
+    @BeforeEach
+    void setUp() {
+        emailValidator = new EmailValidator();
+    }
+
+    @Test
+    public void validEmail() {
+        assertDoesNotThrow(emailValidator.validate("mjakaitis18@gmail.com"));
+    }
+
+    @Test
+    public void emailIsNull() {
+        assertThrows(NullEmailException.class, () -> {
+            emailValidator.validate(null);
+        });
+    }
+
+    @Test
+    public void emailDoesNotContainAddressSign() {
+        assertThrows(EmailWithoutAtSymbolException.class, () -> {
+            emailValidator.validate("mjakaitisgmail.com");
+        });
+    }
+
+    @Test
+    public void emailContainsInvalidSymbols() {
+        assertThrows(EmailContainsInvalidSymbolsException.class, () -> {
+            emailValidator.validate("m.jakait$%$/is99@gmail.com");
+        });
+    }
+
+    @Test
+    public void emailHasInvalidDomain() {
+        assertThrows(EmailHasInvalidDomainException.class, () -> {
+            emailValidator.validate("mjakaitis18@gmail.co-%kk");
+        });
+    }
+}
---
+++ ValidationModule/test/PhoneValidatorTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PhoneValidatorTest {
+
+    PhoneValidator phoneValidator;
+    Map<String, ValidationInstruction> validationInstructionMap;
+
+    @BeforeEach
+    void setUp() {
+        phoneValidator = new PhoneValidator();
+        validationInstructionMap = new HashMap<>();
+    }
+
+    @Test
+    void validPhoneNumber() {
+        ValidationInstruction validationInstructionLT = new ValidationInstruction("9", null);
+        validationInstructionMap.put("INSTRUCTIONLT", validationInstructionLT);
+
+        assertDoesNotThrow(phoneValidator.validate("864256951", "INSTRUCTIONLT"));
+    }
+
+    @Test
+    void validPhoneNumberWithoutInstruction() {
+        assertDoesNotThrow(phoneValidator.validate("864256951"));
+    }
+
+    @Test
+    void validPhoneNumberWithPrefix() {
+        ValidationInstruction validationInstructionLT = new ValidationInstruction("9", "+370");
+        validationInstructionMap.put("INSTRUCTIONLT", validationInstructionLT);
+
+        assertDoesNotThrow(phoneValidator.validate("+37065211512", "INSTRUCTIONLT"));
+    }
+
+    @Test
+    public void phoneIsNull() {
+        assertThrows(NullPhoneException.class, () -> {
+            phoneValidator.validate(null);
+        });
+    }
+
+    @Test
+    public void phoneContainsInvalidSymbols() {
+        ValidationInstruction validationInstructionNo2 = new ValidationInstruction("12", "+320");
+        validationInstructionMap.put("INSTRUCTION2", validationInstructionNo2);
+
+        assertThrows(InvalidEmailSymbolException.class, () -> {
+            phoneValidator.validate("+320211AAB154", "INSTRUCTION2");
+        });
+    }
+
+    @Test
+    public void phoneIsOfInvalidLength() {
+        ValidationInstruction validationInstructionNo2 = new ValidationInstruction("12", "+320");
+        validationInstructionMap.put("INSTRUCTION2", validationInstructionNo2);
+
+        assertThrows(InvalidEmailLengthException.class, () -> {
+            phoneValidator.validate("8621118A", "INSTRUCTION2");
+        });
+    }
+}
